cf311c474c348e35244e1e34a66e958d7d45abeb
switters <codenomadic@gmail.com>
2014-03-25 14:27:52 -0700

    the second first commit.

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# retsly-js-leads
+
+  UI for creating leads on Retsly
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install retsly/retsly-js-leads
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = Leads;
+//var Emitter = require('emitter');
+//Emitter(Leads.prototype);
+
+function Leads() {
+  //this.on('do', this.doSomething);
+};
+
+Leads.prototype.dySomething = function() {
+  console.log('something);
+};
+
+//var L = new Leads();
+//L.emit('do', { data: 'test' });
