ce114e45238535e8c16180fa9353209d28c8eb62
Ali Yazdani <aliyazdani82@gmail.com>
2014-04-08 14:32:06 -0700

    refactored to pass in values including hidden fields

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 /**
  * Leads Component
  */
-var $ = require('jquery');
+var $ = require('jquery')
+  , _ = require('underscore')
+  , Backbone = require('backbone')
+  , validate = require('validate-form')
+  , template = require('./templates/template');
+  
 require('jquery.cookie');
-var _ = require('underscore');
-var Backbone = require('backbone');
-var template = require('./templates/template');
-var validate = require('validate-form');
-
 Backbone.$ = $;
 
 module.exports = function(opts) {
 
+  if(!opts) throw new Error('no options passed in');
+  if(!opts.domain) throw new Error('no domain passed in');
+  if(!opts.agent_id) throw new Error('no agent_id passed in');
+  if(!opts.vendor_id) throw new Error('no vendor_id passed in');
+  if(!opts.listing_id) throw new Error('no listing_id passed in');
+  if(!opts.retsly) throw new Error('no instance of Retsly passed in');
+
   var Components = {};
   var domain = opts.domain;
-  var token = opts.token;
   var retsly = opts.retsly;
+  var token = retsly.client_id;
+  var agent_id = opts.agent_id;
+  var vendor_id = opts.vendor_id;
+  var listing_id = opts.listing_id;
 
   /**
    * Sets up the backbone view and pushes in the template.
@@ -29,9 +39,9 @@ module.exports = function(opts) {
     initialize: function(opts){
 
       var opts = $.merge(opts, {});
-
       if(!opts || typeof opts === 'undefined')
         throw new Error('form could not be loaded');
+      if(!opts.el) throw new Error('no page el passed in');
 
       $(opts.el).append(this.$el);
       this.$el.html(template);
@@ -62,8 +72,11 @@ module.exports = function(opts) {
     submit: function(evt) {
 
       evt.preventDefault();
-
       this.validateform();
+
+      $('form #listingID').val(opts.listing_id);
+      $('form #vendorID').val(opts.vendor_id);
+      $('form #agentID').val(opts.agent_id);
 
       this.form.validateAll(function(err, valid, msg) {
         if(!valid) {
@@ -71,11 +84,12 @@ module.exports = function(opts) {
         }
         else {
           var data = $('#lead').serialize();
+          var url = domain+"/api/v1/lead/?access_token="+token+"&origin=http://"+document.domain;
 
           $.ajax({
             type: 'POST',
             data: data,
-            url: domain+"/api/v1/lead/?access_token="+token+"&origin=http://"+document.domain,
+            url: url,
             xhrFields: { withCredentials: true },
   	        crossDomain: true,
             beforeSend: function( xhr ) {
@@ -88,6 +102,7 @@ module.exports = function(opts) {
             },
             error: function (xhr,err) {throw new Error(err);}
           });
+
         }
       });
     },
